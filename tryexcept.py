a969655bc7c0a292e45c6d201f1a9a4f699bd611
yanvictorsn <yanvictosn@gmail.com>
2023-02-13 22:05:29 -0300

    adding try except exercise

---
+++ tryexcept.py
@@ -0,0 +1,15 @@
+while True:
+    try:
+        number = int(input("Digite um número para dividí-lo por um:"))
+        result = 1/number
+    except ZeroDivisionError:
+        print("Digite um número diferente de Zero")
+    except ValueError:
+        print('Digite um número válido.')
+    except Exception as err:
+        print(f"Unexpected {err=}")
+    else:
+        print(result)
+        break
+    finally:
+        print("Yan Victor Sampaio do Nascimento.")
